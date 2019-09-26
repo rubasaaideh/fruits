7d8bd349a8688a2baff491bdecce16871d823e41
rubasaaideh <ruba457@gmail.com>
2019-09-26 13:41:56 +0300

    okkk

---
+++ js/apple.js
@@ -0,0 +1,11 @@
+var helpyou =confirm("can help you to order apple or orangchoosing for");
+var output;
+
+if (helpyou == true) {
+    output = 'contant us to help you';
+}else
+{ output ='visit our website and hope you enjoy'
+}
+
+document.write('<h1>' + output + '</h1>')
+    
